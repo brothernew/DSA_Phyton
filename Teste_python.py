962ba2539268f8377923c84139ef1ba73d32c13a
ThiagoRP <thirp.impacta@gmail.com>
2022-04-14 14:02:53 -0300

    Primeiro commit

---
+++ Teste_python.py
@@ -0,0 +1,13 @@
+lista = ['Laranja', 'Maça', 'Abacaxi', 'Uva', 'Morango']
+
+for fruta in lista:
+
+    fru = (input("Digite uma fruta "))
+
+    if fruta == fru:
+        print(fruta + " faz parte da lista de frutas")
+
+    elif fruta != fru:
+        print(fru + " não faz parte da lista de frutas")
+
+    break
